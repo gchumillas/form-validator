ecf501c5f2697157bddf0bb808a2bf7566db2277
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2021-04-04 16:19:39 +0200

    refactor

--- lib/src/index.test.ts
+++ lib/src/index.test.ts
@@ -1,24 +1,36 @@
 import { renderHook, act } from '@testing-library/react-hooks'
 import { useValidator } from './index'
 
-test('testing useValidator', () => {
+test('There is an invalid field', () => {
   const { result } = renderHook(() =>
     useValidator({ defaultValidator: val => !!val || 'Required field' })
   )
 
-  describe('testing defaultValidator', () => {
-    const item = {
-      username: 'John',
-      password: ''
-    }
+  act(() =>
+    expect(
+      result.current.test({
+        username: 'John',
+        password: ''
+      })
+    ).toBe(false)
+  )
+  expect(result.current.text('username')).toBe(undefined)
+  expect(result.current.text('password')).toBe('Required field')
+})
 
-    // test('invalid field')
-    act(() => expect(result.current.test(item)).toBe(false))
-    expect(result.current.text('password')).toBe('Required field')
+test('All fields are OK', () => {
+  const { result } = renderHook(() =>
+    useValidator({ defaultValidator: val => !!val || 'Required field' })
+  )
 
-    // test('all fields are OK')
-    act(() => expect(result.current.test({ ...item, password: 'xxx' })).toBe(true))
-    expect(result.current.text('username')).toBe(undefined)
-    expect(result.current.text('password')).toBe(undefined)
-  })
+  act(() =>
+    expect(
+      result.current.test({
+        username: 'John',
+        password: 'xxx'
+      })
+    ).toBe(true)
+  )
+  expect(result.current.text('username')).toBe(undefined)
+  expect(result.current.text('password')).toBe(undefined)
 })
