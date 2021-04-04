f234d01f0a2962dc28318564c28dc9b5718b9cdd
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2021-04-04 16:52:04 +0200

    refactor

--- lib/src/index.test.ts
+++ lib/src/index.test.ts
@@ -1,17 +1,34 @@
 import { renderHook, act } from '@testing-library/react-hooks'
 import { useValidator } from './index'
 
-test('defaultValidator', () => {
-  const item = { username: 'John', password: '' }
+const errors = {
+  requiredFields: 'Required fields',
+  doNotMatch: 'Passwords do not match',
+  unknownGender: 'Unknown gender'
+}
+
+test('validators', () => {
+  const item = { username: 'John', password: '', rePassword: '', gender: '' }
   const { result } = renderHook(() =>
-    useValidator({ defaultValidator: val => !!val || 'Required field' })
+    useValidator({
+      defaultValidator: val => !!val || errors.requiredFields,
+      validators: defaultValidator => ({
+        rePassword: [defaultValidator, val => val == 'xxx' || errors.doNotMatch],
+        gender: [
+          val => !val || ['male', 'female', 'butterfly'].includes(val) || errors.unknownGender
+        ]
+      })
+    })
   )
 
   act(() => expect(result.current.test(item)).toBe(false))
   expect(result.current.text('username')).toBeUndefined()
-  expect(result.current.text('password')).toBe('Required field')
+  expect(result.current.text('password')).toBe(errors.requiredFields)
+  expect(result.current.text('rePassword')).toBe(errors.requiredFields)
+  expect(result.current.text('gender')).toBeUndefined()
 
-  act(() => expect(result.current.test({ ...item, password: 'xxx' })).toBe(true))
-  expect(result.current.text('username')).toBeUndefined()
-  expect(result.current.text('password')).toBeUndefined()
+  act(() =>
+    expect(result.current.test({ ...item, password: 'xxx', rePassword: 'yyyy' })).toBe(false)
+  )
+  expect(result.current.text('rePassword')).toBe(errors.doNotMatch)
 })
