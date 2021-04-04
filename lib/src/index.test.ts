6974f45381eea3039a3d359e164e9b3a93adcae5
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2021-04-04 16:36:34 +0200

    refactor

--- lib/src/index.test.ts
+++ lib/src/index.test.ts
@@ -1,36 +1,17 @@
 import { renderHook, act } from '@testing-library/react-hooks'
 import { useValidator } from './index'
 
-test('There is an invalid field', () => {
+test('defaultValidator', () => {
+  const item = { username: 'John', password: '' }
   const { result } = renderHook(() =>
     useValidator({ defaultValidator: val => !!val || 'Required field' })
   )
 
-  act(() =>
-    expect(
-      result.current.test({
-        username: 'John',
-        password: ''
-      })
-    ).toBe(false)
-  )
+  act(() => expect(result.current.test(item)).toBe(false))
   expect(result.current.text('username')).toBe(undefined)
   expect(result.current.text('password')).toBe('Required field')
-})
 
-test('All fields are OK', () => {
-  const { result } = renderHook(() =>
-    useValidator({ defaultValidator: val => !!val || 'Required field' })
-  )
-
-  act(() =>
-    expect(
-      result.current.test({
-        username: 'John',
-        password: 'xxx'
-      })
-    ).toBe(true)
-  )
+  act(() => expect(result.current.test({ ...item, password: 'xxx' })).toBe(true))
   expect(result.current.text('username')).toBe(undefined)
   expect(result.current.text('password')).toBe(undefined)
 })
